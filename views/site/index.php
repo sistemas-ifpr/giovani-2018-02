d8e68404ebc3f5a10adf8d48267b7545fc93b3a0
Giovani Wolff <giovani7wolff@gmail.com>
2018-08-03 22:18:05 -0300

    Entrega 01

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Bem Vindo</h1>
+
+        <p class="lead">Doppler?</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Começe com Yii</a></p>
+    </div>
+
+</div>
